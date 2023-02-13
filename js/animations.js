a80fabe6b4737ddb3d46a744a8c94cbdf05dbc5a
Trybuu <80851501+Trybuu@users.noreply.github.com>
2023-02-13 22:29:33 +0100

    Animation bug fixing

--- js/animations.js
+++ js/animations.js
@@ -1,14 +1,26 @@
-gsap.registerPlugin('ScrollTrigger')
+gsap.registerPlugin("ScrollTrigger");
 
-const timeline = gsap.timeline({defaults: {duration: 1, opacity: 0, scale: 0}});
+const timeline = gsap.timeline({
+  defaults: { duration: 1, opacity: 0, scale: 0 },
+});
 timeline
-    .from('.main__content__title', {ease: 'back'})
-    .from('.main__content__para', {ease: 'sine'})
-    .from('.main__content__subtitle', {ease: 'sine'}, '<')
-    .from('.main__content__button', {ease: 'power1'})
-    .from('.article__img-wrapper__img', {})
-    ;
+  .from(".main__content__title", { ease: "back" })
+  .from(".main__content__para", { ease: "sine" })
+  .from(".main__content__subtitle", { ease: "sine" }, "<")
+  .from(".main__content__button", { ease: "power1" })
+  .from(".article__img-wrapper__img", {});
 
-gsap.from('.article__img-wrapper__img', {rotation:"360", duration: 10,  ease:Linear.easeNone, repeat:-1});
+gsap.from(".article__img-wrapper__img", {
+  rotation: "360",
+  duration: 10,
+  ease: Linear.easeNone,
+  repeat: -1,
+});
 
-gsap.from('.footer__content', {scrollTrigger: '.footer', duration: 1, x: '100%', opacity: 0, ease: 'back'})
+gsap.from(".footer__content", {
+  scrollTrigger: ".footer",
+  duration: 1,
+  y: "-50%",
+  opacity: 0,
+  ease: "power1",
+});
